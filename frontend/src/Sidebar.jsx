427f15bc90b21fc462416823b0bd804ef5e70b1f
Rahul Patel <140371157+appy-spec@users.noreply.github.com>
2026-06-04 16:16:15 +0530

    update for the responsiveness

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -1,6 +1,6 @@
 import "./Sidebar.css";
 import { MyContext } from "./MyContext.jsx";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { v1 as uuidv1 } from "uuid";
 
 function Sidebar() {
@@ -13,17 +13,23 @@ function Sidebar() {
     setPrevChat,
     setReply,
     setCurrThreadId,
+    showSidebar,
+    setShowSidebar
   } = useContext(MyContext);
 
   const getAllThreads = async () => {
     try {
-      const response = await fetch("http://localhost:8080/api/thread");
+      const response = await fetch(
+        "https://sigma-gpt-9ujx.onrender.com/api/thread"
+      );
+
       const res = await response.json();
 
       const filterData = res.map((thread) => ({
         threadId: thread.threadId,
         title: thread.title,
       }));
+
       setAllThreads(filterData.reverse());
     } catch (err) {
       console.log(err);
@@ -35,6 +41,8 @@ function Sidebar() {
   }, [currThreadId]);
 
   const createNewChat = () => {
+    setShowSidebar(false);
+
     setNewChat(true);
     setPrompt("");
     setReply(null);
@@ -43,14 +51,18 @@ function Sidebar() {
   };
 
   const changeThread = async (threadId) => {
+    setShowSidebar(false);
+
     setReply(null);
     setNewChat(false);
 
     try {
       const response = await fetch(
-        `http://localhost:8080/api/thread/${threadId}`,
+        `https://sigma-gpt-9ujx.onrender.com/api/thread/${threadId}`
       );
+
       const res = await response.json();
+
       setPrevChat(res.message);
       setCurrThreadId(res.threadId);
     } catch (err) {
@@ -61,12 +73,16 @@ function Sidebar() {
   const deleteThread = async (threadId) => {
     try {
       const response = await fetch(
-        `http://localhost:8080/api/thread/${threadId}`,
-        { method: "DELETE" },
+        `https://sigma-gpt-9ujx.onrender.com/api/thread/${threadId}`,
+        {
+          method: "DELETE",
+        }
       );
-      const res = await response.json();
+
+      await response.json();
+
       setAllThreads((prev) =>
-        prev.filter((thread) => thread.threadId != threadId),
+        prev.filter((thread) => thread.threadId !== threadId)
       );
 
       if (threadId === currThreadId) {
@@ -78,38 +94,44 @@ function Sidebar() {
   };
 
   return (
-    <section className="sidebar">
-      <button onClick={createNewChat}>
-        <img
-          src="src/assets/blacklogo.png"
-          alt="gpt logo"
-          className="logo"
-        ></img>
-        <span>
-          <i className="fa-solid fa-pen-to-square"></i>
-        </span>
-      </button>
+    <>
+      <section className={`sidebar ${showSidebar ? "active" : ""}`}>
+        <button onClick={createNewChat}>
+          <img
+            src="src/assets/blacklogo.png"
+            alt="gpt logo"
+            className="logo"
+          />
+          <span>
+            <i className="fa-solid fa-pen-to-square"></i>
+          </span>
+        </button>
 
-      <ul className="history">
-        {allThreads?.length > 0 &&
-          allThreads.map((thread, idx) => (
-            <li key={idx} onClick={(e) => changeThread(thread.threadId)}>
-              {thread.title}&nbsp;&nbsp;&nbsp;&nbsp;
-              <i
-                className="fa-solid fa-trash"
-                onClick={(e) => (
-                  e.stopPropagation(),
-                  deleteThread(thread.threadId)
-                )}
-              ></i>
-            </li>
-          ))}
-      </ul>
+        <ul className="history">
+          {allThreads?.length > 0 &&
+            allThreads.map((thread) => (
+              <li
+                key={thread.threadId}
+                onClick={() => changeThread(thread.threadId)}
+              >
+                {thread.title}&nbsp;&nbsp;&nbsp;&nbsp;
 
-      <div className="sign">
-        <p>By Rahul &hearts;</p>
-      </div>
-    </section>
+                <i
+                  className="fa-solid fa-trash"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    deleteThread(thread.threadId);
+                  }}
+                ></i>
+              </li>
+            ))}
+        </ul>
+
+        <div className="sign">
+          <p>By Rahul &hearts;</p>
+        </div>
+      </section>
+    </>
   );
 }
 
